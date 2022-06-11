d94b79e2c46806fc6c55cf994b5a00ba52575d9d
Kimberly <kimolanga123@gmail.com>
2022-06-11 23:25:03 +0300

    update views.py

--- app/models.py
+++ app/models.py
@@ -1,3 +1,54 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.forms import ImageField, ModelForm
+from django import forms
 
 # Create your models here.
+class Project(models.Model):
+    """
+    This class takes care of the posted projects
+    """
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='projects')
+
+    image = ImageField('image')
+    title = models.CharField(max_length=50)
+    description=models.CharField(max_length=100)
+    url = models.TextField()
+    posted_date = models.DateTimeField(auto_now_add=True)
+    rate = models.IntegerField(default=0)
+
+    @classmethod
+    def get_project_by_user(cls, user):
+        project = cls.objects.filter(user=user)
+        return project
+
+    def save_project(self):
+        self.save()
+
+    def delete_project(self):
+        self.delete()
+
+    #  get by id
+    @classmethod
+    def get_one_project(cls, id):
+        project = cls.objects.get(id=id)
+        return project
+
+    
+    @classmethod
+    def search_by_title(self, search_title):
+        
+        projects = Project.objects.filter(title__icontains=search_title)
+        return projects
+  
+
+    def __str__(self):
+        return self.user.username       
+
+class AddProjectForm(ModelForm):
+    class Meta:
+        model = Project
+        fields = ['title','description','image','url']
+        widgets= {
+            'url':forms.Textarea(attrs={'rows':2,})
+        }
--- app/views.py
+++ app/views.py
@@ -1,12 +1,25 @@
-from django.shortcuts import render
-from django.contrib.auth.decorators import login_required
+from django.shortcuts import render,redirect
+from .models import AddProjectForm, Project
+from django.contrib import messages
 
 # Create your views here.
 def homepage(request):
 
     return render(request,'index.html')
 
-@login_required
+
 def new_post(request):
+    project=Project.objects.all()
+    if request.method=='POST':
+        current_user=request.user
+        form=AddProjectForm(request.POST,request.FILES)
+        if form.is_valid():
+            project=form.save(commit=False)
+            project.user=current_user
+            project.save()
+            messages.success(request,('Project was posted successfully!'))
+            return redirect('home')
+    else:
+            form=AddProjectForm()
 
     return render(request,'new-post.html')
