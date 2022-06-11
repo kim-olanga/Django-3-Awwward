decb138e456f219865ba938fd55ade8aa33c437f
Kimberly <kimolanga123@gmail.com>
2022-06-11 22:35:52 +0300

    import login-required

--- app/views.py
+++ app/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def homepage(request):
 
     return render(request,'index.html')
 
+@login_required
 def new_post(request):
 
     return render(request,'new-post.html')
