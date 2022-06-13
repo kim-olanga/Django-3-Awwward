7f444d491be9f4fa6444001e466926ba6e5b0ba9
Kimberly <kimolanga123@gmail.com>
2022-06-13 10:35:07 +0300

    create super user

--- app/views.py
+++ app/views.py
@@ -1,6 +1,4 @@
-from django.shortcuts import render,redirect
-from .models import AddProjectForm, Project
-from django.contrib import messages
+from django.shortcuts import render
 
 # Create your views here.
 def homepage(request):
@@ -8,17 +6,5 @@ def homepage(request):
     return render(request,'index.html')
 
 def new_post(request):
-    project=Project.objects.all()
-    if request.method=='POST':
-        current_user=request.user
-        form=AddProjectForm(request.POST,request.FILES)
-        if form.is_valid():
-            project=form.save(commit=False)
-            project.user=current_user
-            project.save()
-            messages.success(request,('Project was posted successfully!'))
-            return redirect('home')
-    else:
-            form=AddProjectForm()
 
     return render(request,'new-post.html')
