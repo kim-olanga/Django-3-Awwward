66e4ed3a62af690dc2248d300818cc56e09744a9
Kimberly <kimolanga123@gmail.com>
2022-06-13 09:57:35 +0300

    update settings.py

--- award/settings.py
+++ award/settings.py
@@ -38,7 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'app',
-    'bootstrap4'
+    'bootstrap4',
+    'crispy_forms',
+    'tinymce',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
