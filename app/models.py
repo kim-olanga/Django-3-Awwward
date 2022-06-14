98e3693e3d3ca817b373955d275bd770cae295a8
Kimberly <kimolanga123@gmail.com>
2022-06-14 07:41:32 +0300

    create database table for class profile

--- app/models.py
+++ app/models.py
@@ -1,4 +1,27 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.dispatch import receiver
+from django.db.models.signals import post_save
+from tinymce.models import HTMLField
 
 # Create your models here.
+class Profile(models.Model):
+    profile_pic = models.ImageField( upload_to='profile/', blank ='true',default='default.png')
+    bio = models.TextField()
+    user =models.OneToOneField(User, on_delete = models.CASCADE)
+    date_craeted= models.DateField(auto_now_add=True )
+
+    def __str__(self):
+        return f'{self.user.username} Profile'
+
+    def save_profile(self):
+        self.save
+    
+    def delete_user(self):
+        self.delete()
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+
+    if created:
+        Profile.objects.create(user=instance)
