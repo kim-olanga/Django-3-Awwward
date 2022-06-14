6e0ec696514134352f4c658988bd89b1332e6f3c
Kimberly <kimolanga123@gmail.com>
2022-06-14 07:42:47 +0300

    update models.py by creating class project

--- app/models.py
+++ app/models.py
@@ -25,3 +25,28 @@ def create_user_profile(sender, instance, created, **kwargs):
 
     if created:
         Profile.objects.create(user=instance)
+
+class Project(models.Model):
+    title = models.TextField(max_length=30)
+    image = models.ImageField(upload_to = 'home/', blank=True)
+    link= models.URLField(max_length=200)
+    description = models.TextField(max_length=300)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, default='', null=True ,related_name='author')
+    date_craeted= models.DateField(auto_now_add=True )
+    
+    def save_project(self):
+        self.save()
+
+    @classmethod
+    def all_projects(cls) :
+        projects = cls.objects.all()
+        return projects
+
+    @classmethod
+    def search_by_title(cls,search_term):
+        projects = cls.objects.filter(title__icontains=search_term)
+        return projects
+
+    def __str__(self):
+        return self.title
+
