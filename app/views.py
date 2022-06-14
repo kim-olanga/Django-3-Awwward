d387aa2f742860464276bd7b56514e65594705c1
Kimberly <kimolanga123@gmail.com>
2022-06-14 07:38:06 +0300

    import user from auth contrib

--- app/views.py
+++ app/views.py
@@ -2,10 +2,28 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 
 # Create your views here.
-
+@login_required(login_url='/accounts/login/')
 def homepage(request):
+    project= Project.all_projects()
+    json_projects = []
+    for project in project:
 
-    return render(request,'index.html')
+        pic = Profile.objects.filter(user=project.user.id).first()
+        if pic:
+            pic = pic.profile_pic.url
+        else:
+            pic =''
+        obj = dict(
+            title=project.title,
+            image=project.image,
+            link=project.link,
+            description=project.description,
+            avatar=pic,
+            date_craeted=project.date_craeted,
+            author=project.user.username  
+        )
+        json_projects.append(obj)
+    return render(request,'index.html',{"json_projects":json_projects})
 
 def new_post(request):
 
