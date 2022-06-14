b30c89fb6c64d572cafee9789647d7172ca7d4fd
Kimberly <kimolanga123@gmail.com>
2022-06-14 08:08:24 +0300

    update the project urls.py

--- app/urls.py
+++ app/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
 from . import views
+from app import views as user_views
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns=[
     path('',views.homepage,name='homepage'),
     path('accounts/register/', views.register, name='register'),
     path('new-post/',views.new_post,name='new-post'),
 ]
+
+if settings.DEBUG:
+       urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
