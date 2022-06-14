fbe027dc6b42f42bf7d86dcfef688a54a633eef6
Kimberly <kimolanga123@gmail.com>
2022-06-14 07:22:22 +0300

    create database

--- award/settings.py
+++ award/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -80,8 +81,10 @@ WSGI_APPLICATION = 'award.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'award',
+        'USER':'kimberly',
+        'PASSWORD':'kim12345',
     }
 }
 
